70ca89b50097c000348b819221d97bc2eee27607
Urvish Patel <92674497+UrvishP4503@users.noreply.github.com>
2024-07-08 21:20:28 +0530

    formatted code

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,17 +1,17 @@
 import {SignedIn, SignedOut, SignInButton, UserButton} from "@clerk/nextjs";
 
 export function Topnav() {
-    return (
-        <nav className="flex items-center justify-between w-full border-b p-4 text-xl font-semibold">
-            <div>Hey</div>
-            <div>
-                <SignedOut>
-                    <SignInButton/>
-                </SignedOut>
-                <SignedIn>
-                    <UserButton/>
-                </SignedIn>
-            </div>
-        </nav>
-    )
+  return (
+    <nav className="flex items-center justify-between w-full border-b p-4 text-xl font-semibold">
+      <div>Hey</div>
+      <div>
+        <SignedOut>
+          <SignInButton/>
+        </SignedOut>
+        <SignedIn>
+          <UserButton/>
+        </SignedIn>
+      </div>
+    </nav>
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,35 +4,35 @@ import {SignedIn, SignedOut} from "@clerk/nextjs";
 export const dynamic = "force-dynamic";
 
 async function Images() {
-    const posts = await db.query.images.findMany({
-        orderBy: (model, {desc}) => desc(model.id),
-    });
-    return (
-        <div>
-            <div className="flex flex-wrap gap-4">
-                {posts.map((post) => (
-                    <div key={post.id} className="flex w-48 flex-col">
-                        <img src={post.url} alt={post.name}/>
-                    </div>
-                ))}
-            </div>
-            Hello This in still in progress
-        </div>
-    )
+  const posts = await db.query.images.findMany({
+    orderBy: (model, {desc}) => desc(model.id),
+  });
+  return (
+    <div>
+      <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id} className="flex w-48 flex-col">
+            <img src={post.url} alt={post.name}/>
+          </div>
+        ))}
+      </div>
+      Hello This in still in progress
+    </div>
+  )
 }
 
 export default async function HomePage() {
-    return (
-        <main className="">
-            <SignedOut>
-                <div className="w-full h-full text-2xl text-center">
-                    Please sign in
-                </div>
-            </SignedOut>
-            <SignedIn>
-                <Images/>
-            </SignedIn>
+  return (
+    <main className="">
+      <SignedOut>
+        <div className="w-full h-full text-2xl text-center">
+          Please sign in
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images/>
+      </SignedIn>
 
-        </main>
-    );
+    </main>
+  );
 }
