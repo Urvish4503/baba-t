a6da4ea359c6a9b2b6bb080d695ac53a88a9b4ff
Urvish Patel <92674497+UrvishP4503@users.noreply.github.com>
2024-07-08 21:14:33 +0530

    added auth things

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,38 @@
-import { db } from "~/server/db";
+import {db} from "~/server/db";
+import {SignedIn, SignedOut} from "@clerk/nextjs";
 
 export const dynamic = "force-dynamic";
 
+async function Images() {
+    const posts = await db.query.images.findMany({
+        orderBy: (model, {desc}) => desc(model.id),
+    });
+    return (
+        <div>
+            <div className="flex flex-wrap gap-4">
+                {posts.map((post) => (
+                    <div key={post.id} className="flex w-48 flex-col">
+                        <img src={post.url} alt={post.name}/>
+                    </div>
+                ))}
+            </div>
+            Hello This in still in progress
+        </div>
+    )
+}
+
 export default async function HomePage() {
-  const posts = await db.query.images.findMany({
-    orderBy: (model, { desc }) => desc(model.id),
-  });
-  return (
-    <main className="">
-      <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id} className="flex w-48 flex-col">
-            <img src={post.url}  alt={post.name}/>
-          </div>
-        ))}
-      </div>
-      Hello This in still in progress
-    </main>
-  );
+    return (
+        <main className="">
+            <SignedOut>
+                <div className="w-full h-full text-2xl text-center">
+                    Please sign in
+                </div>
+            </SignedOut>
+            <SignedIn>
+                <Images/>
+            </SignedIn>
+
+        </main>
+    );
 }
